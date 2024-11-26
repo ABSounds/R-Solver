d2cc9003e8a4c379b2d1e3d42aba69b16fa35083
Alberto Barrera <130357837+ABSounds@users.noreply.github.com>
2024-11-26 08:25:03 +0100

    Update matrix_helpers.py to use sympy

--- r_solver_utils/matrix_helpers.py
+++ r_solver_utils/matrix_helpers.py
@@ -1,4 +1,4 @@
-from sage.all import matrix, SR, identity_matrix, solve
+from sympy import Matrix, symbols, eye, solve, simplify
 
 from r_solver_utils.element_stamps import stamp_element
 from r_solver_utils.element_stamps import RES_TYPE
@@ -6,54 +6,68 @@ from r_solver_utils.element_stamps import RES_TYPE
 
 def construct_X_matrix(elements, num_nodes, num_ports, num_extras):
     '''Constructs an \'X\' Matrix for doing MNA'''
-    X_mat = matrix(SR, num_nodes + num_ports + num_extras, num_nodes + num_ports + num_extras)
+    X_mat = Matrix.zeros(num_nodes + num_ports + num_extras, num_nodes + num_ports + num_extras)
+
+    print(f'X matrix shape: {X_mat.shape}')
 
     for el in elements:
         X_mat = stamp_element(X_mat, el, num_nodes, num_ports)
-    
+
     return X_mat
 
 
 def remove_datum_node(X_mat, datum):
-    matrix_range = [x for x in range(X_mat.nrows()) if x != datum]
-    return matrix(X_mat[matrix_range, matrix_range])
+    matrix_range = [x for x in range(X_mat.shape[0]) if x != datum]
+    return X_mat[matrix_range, :][:, matrix_range]  # SymPy handles the slicing 
 
 
 def compute_S_matrix(X_inv, elements, num_ports, num_extras):
     start_ports = -(num_ports + num_extras)
-    end_ports = -num_extras if num_extras > 0 else X_inv.ncols()
+    end_ports = -num_extras if num_extras > 0 else X_inv.shape[1]
 
-    vert_id = matrix(SR, X_inv.nrows(), num_ports)
-    vert_id[start_ports:end_ports, :] = identity_matrix(num_ports)
+    # Create symbolic identity matrices
+    vert_id = Matrix.zeros(X_inv.shape[0], num_ports)
+    vert_id[start_ports:end_ports, :] = eye(num_ports)
 
-    hor_id = matrix(SR, num_ports, X_inv.ncols())
-    hor_id[:, start_ports:end_ports] = identity_matrix(num_ports)
+    hor_id = Matrix.zeros(num_ports, X_inv.shape[1])
+    hor_id[:, start_ports:end_ports] = eye(num_ports)
 
-    Rp_diag = matrix(SR, num_ports, num_ports)
+    # Construct the Rp diagonal matrix
+    Rp_diag = Matrix.zeros(num_ports, num_ports)
     for el in elements:
-        if el.type == RES_TYPE:
+        if el.type == 'RES':
             Rp_diag[el.port, el.port] = el.impedance
 
-    S_mat = identity_matrix(num_ports) + 2 * Rp_diag * hor_id * X_inv * vert_id
+    # Compute the scattering matrix S
+    S_mat = eye(num_ports) + 2 * Rp_diag * hor_id * X_inv * vert_id
 
     return S_mat, Rp_diag
 
 
 def adapt_port(S_mat, Rp, port):
     '''Adapts one port of the scattering matrix to be a reflection-free port'''
-    if port < 0 or port >= S_mat.nrows():
+    if port < 0 or port >= S_mat.shape[0]:
         raise IndexError('Port index must be less than the number of available ports')
 
+    # Get the S matrix element at (port, port)
     S_nn = S_mat[port, port]
+    
+    # Get the impedance matrix element at (port, port)
     R_n = Rp[port, port]
 
-    R_n_solves = solve(S_nn == 0, R_n)
-    R_n_solve_expr = R_n_solves[0]
+    # Solve the equation S_nn == 0 for the impedance R_n
+    R_n_solves = solve(S_nn - 0, R_n)
+    R_n_solve_expr = R_n_solves[0]  # Take the first solution (assuming it exists)
 
-    R_n_solved = R_n_solve_expr.right()
+    # Get the solved impedance value (right-hand side of the expression)
+    R_n_solved = R_n_solve_expr.rhs
 
+    # Print the adapted impedance value
     print('')
     print('Adapted Port Impedance:')
     print(R_n_solve_expr)
 
-    return S_mat.substitute({R_n: R_n_solved}), R_n_solve_expr
+    # Substitute the solved impedance back into the scattering matrix
+    S_mat_substituted = S_mat.subs({R_n: R_n_solved})
+
+    return S_mat_substituted, R_n_solve_expr
